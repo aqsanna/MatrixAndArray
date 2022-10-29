6380ab10ae94d24ce9102a60646dd565ede0fc6b
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-10-29 22:02:30 +0400

    Interface

--- src/ExceptionExampl/ЕхсО.java
+++ src/ExceptionExampl/ЕхсО.java
@@ -1,12 +1,12 @@
 package ExceptionExampl;
 
 public class ЕхсО {
-    static  void subRoutin(){
-        try{
-            int d=0;
-            int a = 10/d;
+    static void subRoutin() {
+        try {
+            int d = 0;
+            int a = 10 / d;
             System.out.println(" Этo не будет выведено .");
-        }catch(ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println("Дeлeниe на нуль.");
         }
         System.out.println("Пocлe оператора catch.");
